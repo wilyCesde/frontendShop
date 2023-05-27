67edce1d4212b78411d0d818f59f84a35f4a03b2
wilyCesde <wruizza@cesde.net>
2023-05-27 10:14:56 -0500

    list

--- components/ListCustomer.js
+++ components/ListCustomer.js
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import { styles } from "../assets/styles/styles"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Button } from 'react-native-paper'
+
+
+
 
 export default function ListCustomer() {
+
+
+  const [data, setData] = useState([]);
+
+
+  const getCustomers = async () => {
+    const response = await axios.get(`http://127.0.0.1:3000/api/clientes`)
+    setData(response.data)
+    console.log(data);
+  }
+
+
+  useEffect(() => {
+    //getCustomers();
+    //console.log(data);
+  })
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Lista de Clientes</Text>
-      </View>
-    );
+      <Button
+        style={{ backgroundColor: 'red', marginLeft: 10, marginBottom: 10 }}
+        icon="card-search-outline"
+        mode="contained"
+        onPress={getCustomers}
+      >
+        Listar Clientes
+      </Button>
+      <Text>Listado de Clientes</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.nombre}{item.apellido}
+          </Text>}
+      />
+    </View>
+  );
 }
