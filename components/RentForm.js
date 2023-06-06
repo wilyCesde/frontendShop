4636fabcef5f5ab24c5aaccfff302f0271f7665d
wilyCesde <wruizza@cesde.net>
2023-06-05 21:18:26 -0500

    parace bien

--- components/RentForm.js
+++ components/RentForm.js
@@ -2,19 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import axios from 'axios';
 
-
-
-
 function RentForm() {
   const [cars, setCars] = useState([]);
-  const [selectedCar, setSelectedCar] = useState(null);
+  const [selectedCar, setSelectedCar] = useState('');
   const [clients, setClients] = useState([]);
-  const [selectedClient, setSelectedClient] = useState(null);
+  const [selectedClient, setSelectedClient] = useState('');
   const [plateNumber, setPlateNumber] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
-  const [rentNumber,setrentNumber] = useState("");
-  const [username,setusername]=useState("");
+  const [rentNumber, setRentNumber] = useState('');
 
   useEffect(() => {
     // Obtener la lista de vehículos al cargar el componente
@@ -40,6 +36,7 @@ function RentForm() {
       console.log(error);
     }
   };
+
   const handleSelectCar = (car) => {
     if (car.state === 'disponible') {
       setSelectedCar(car);
@@ -49,106 +46,98 @@ function RentForm() {
       setErrorMessage('El vehículo seleccionado no está disponible para alquilar');
     }
   };
-  
 
   const handleSelectClient = (client) => {
     setSelectedClient(client);
-    setPlateNumber(client.username) ;
+    setPlateNumber(client.username);
   };
 
   const handleRent = async () => {
-    
-    if(rentNumber,plateNumber,username === ''){
-      setErrorMessage("campos obligatorios");
+    if (rentNumber === '' || plateNumber === '' || selectedCar === null || selectedClient === null) {
+      setErrorMessage('Por favor, completa todos los campos obligatorios');
+      return;
     }
-
-    if (!selectedCar) {
-      setErrorMessage('Por favor, selecciona un vehículo');
-    
-    }
-
-    if (!selectedClient) {
-      setErrorMessage('Por favor, selecciona un cliente');
-    
-    }
-
+  
     if (selectedCar.state !== 'disponible') {
       setErrorMessage('El vehículo seleccionado no está disponible para alquilar');
-      
+      return;
     }
- 
-
+  
     try {
+      // Obtener la fecha actual
+      const rentalDate = new Date();
+  
       // Actualizar el estado del vehículo a "alquilado" en el frontend
       const updatedCars = cars.map((car) =>
         car._id === selectedCar._id ? { ...car, state: 'alquilado' } : car
       );
       setCars(updatedCars);
-
+  
       // Enviar la solicitud POST para crear el estado del vehículo a "alquilado" en el backend
-
-      await axios.post(`http://127.0.0.1:3000/api/createReturnCars`,{
+      await axios.post(`http://127.0.0.1:3000/api/returnCars/${selectedCar._id}`, {
         rentNumber,
         plateNumber,
-        username
-      } );
-
+        username: selectedClient.username,
+        rentalDate,
+      });
+  
       setSuccessMessage('Vehículo alquilado correctamente');
       setPlateNumber('');
       setSelectedCar(null);
       setSelectedClient(null);
-      
     } catch (error) {
       setErrorMessage('Error al alquilar el vehículo');
       console.log(error);
     }
   };
+  
+
   const handleUpdateState = async (carId) => {
     const updateData = {
-      state: 'disponible'
+      state: 'disponible',
     };
-  
+
     try {
       await axios.put(`http://127.0.0.1:3000/api/cars/${carId}`, updateData);
-  
+
       // Actualizar el estado del vehículo en el frontend
       const updatedCars = cars.map((car) =>
         car._id === carId ? { ...car, state: 'disponible' } : car
       );
       setCars(updatedCars);
-  
+
       setSuccessMessage('Estado del vehículo actualizado correctamente');
     } catch (error) {
       setErrorMessage('Error al actualizar el estado del vehículo');
       console.log(error);
     }
   };
+
   const handleDeleteCar = async (carId) => {
     try {
       await axios.delete(`http://127.0.0.1:3000/api/cars/${carId}`);
-  
+
       // Eliminar el vehículo del estado en el frontend
       const updatedCars = cars.filter((car) => car._id !== carId);
       setCars(updatedCars);
-  
+
       setSuccessMessage('Vehículo eliminado correctamente');
     } catch (error) {
       setErrorMessage('Error al eliminar el vehículo');
       console.log(error);
     }
   };
-  
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Alquilar Vehículo</Text>
       {successMessage ? <Text style={styles.successMessage}>{successMessage}</Text> : null}
       {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
-      <TextInput 
-       style={styles.input}
-       value={username}
-       onChangeText={setrentNumber}
-       placeholder='número de renta'
+      <TextInput
+        style={styles.input}
+        value={rentNumber}
+        onChangeText={setRentNumber}
+        placeholder="Número de renta"
       />
       <FlatList
         data={cars}
@@ -160,10 +149,10 @@ function RentForm() {
           >
             <View style={styles.carItemContent}>
               <View>
-                <Text>{item.plateNumber}</Text>
-                <Text>{item.brand}</Text>
-                <Text>{item.state}</Text>
-                <Text>{item.dailyValue}</Text>
+                <Text style={styles.carInfoText}>Placa: {item.plateNumber}</Text>
+                <Text style={styles.carInfoText}>Marca: {item.brand}</Text>
+                <Text style={styles.carInfoText}>Estado: {item.state}</Text>
+                <Text style={styles.carInfoText}>Valor diario: {item.dailyValue}</Text>
               </View>
               <TouchableOpacity
                 style={styles.updateButton}
@@ -180,8 +169,6 @@ function RentForm() {
             </View>
           </TouchableOpacity>
         )}
-        
-        
       />
       <Text style={styles.sectionTitle}>Seleccionar Cliente</Text>
       <FlatList
@@ -192,8 +179,8 @@ function RentForm() {
             style={[styles.clientItem, selectedClient?._id === item._id ? styles.selectedClientItem : null]}
             onPress={() => handleSelectClient(item)}
           >
-            <Text>{item.nombre}</Text>
-            <Text>{item.apellidos}</Text>
+            <Text style={styles.userInfoText}>Nombre: {item.nombre}</Text>
+            <Text style={styles.userInfoText}>Apellidos: {item.apellidos}</Text>
           </TouchableOpacity>
         )}
       />
@@ -215,9 +202,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 40, 
-    fontSize: 24,
+    paddingHorizontal: 40,
     marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  successMessage: {
+    color: 'green',
+    marginTop: 10,
+    fontSize: 16,
+  },
+  errorMessage: {
+    color: 'red',
+    marginTop: 10,
+    fontSize: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    fontSize: 16,
   },
   carItem: {
     width: '100%',
@@ -229,10 +238,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 10,
   },
-  carItemSeparator: {
-    height: 1,
-    backgroundColor: 'lightgray',
-    marginVertical: 5,
+  carItemContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  carInfoContainer: {
+    flex: 1,
+    marginRight: 10,
+  },
+  carInfoText: {
+    fontSize: 16,
+  },
+  carItemButtons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
   },
   selectedCarItem: {
     backgroundColor: 'yellow',
@@ -251,21 +271,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 10,
   },
-  clientItemSeparator: {
-    height: 1,
-    backgroundColor: 'lightgray',
-    marginVertical: 5,
-  },
   selectedClientItem: {
     backgroundColor: 'yellow',
   },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
+  userInfoText: {
+    fontSize: 16,
   },
   button: {
     width: '100%',
@@ -280,11 +290,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   deleteButton: {
-    width: '100%',
     backgroundColor: 'red',
     borderRadius: 5,
     paddingVertical: 10,
     marginBottom: 10,
+    paddingHorizontal: 10,
   },
   deleteButtonText: {
     color: 'white',
@@ -292,24 +302,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   updateButton: {
-    width: '100%',
     backgroundColor: 'green',
     borderRadius: 5,
     paddingVertical: 10,
     marginBottom: 10,
-  },
-  updateButtonText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  errorMessage: {
-    color: 'red',
-    marginTop: 10,
-  },
-  successMessage: {
-    color: 'green',
-    marginTop: 10,
+    paddingHorizontal: 10,
+    marginRight: 10,
   },
 });
 
